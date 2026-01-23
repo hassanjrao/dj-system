53e88ddbbde45edf457bd8a8b15e0bca9c9fffc4
Hassan <hassanjrao@gmail.com>
2026-01-23 20:04:35 +0500

    added statused

--- database/migrations/2026_01_23_000003_add_assignment_status_foreign_key.php
+++ database/migrations/2026_01_23_000003_add_assignment_status_foreign_key.php
@@ -14,8 +14,6 @@ class AddAssignmentStatusForeignKey extends Migration
      */
     public function up()
     {
-
-        return;
         // make assignment_status nullable
         Schema::table('assignments', function (Blueprint $table) {
             $table->string('assignment_status')->nullable()->change();
