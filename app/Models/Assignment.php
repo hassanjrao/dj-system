eab58c9bccba6d23f1bff1d4ba73b36d50b7db4c
Hassan <hassanjrao@gmail.com>
2026-01-23 17:45:08 +0500

    change logic of music assigned

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -181,8 +181,10 @@ class Assignment extends Model
                 })
                 ->where(function ($subQuery) use ($user) {
                     $subQuery->where('created_by', $user->id)
-                             ->orWhere('assigned_to_id', $user->id)
-                             ->orWhere('assignment_status', 'completed');
+                             ->where(function ($subSubQuery) use ($user) {
+                                $subSubQuery->where('assigned_to_id', $user->id)
+                                             ->orWhere('assignment_status', 'completed');
+                             });
                 });
             });
         });
